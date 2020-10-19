d67bda39ffdae6752632cca1298fad66c007c6b5
ono-hikaru81 <Onohikaru0801@gmail.com>
2020-10-19 23:25:08 +0900

    タイトル画面でNewGameのボタンのみをEnterで選択出来るように設定。(Contineも選択できるようにする必要有)

--- RunGame_dangomusi/Assets/Title/SceneController.cs
+++ RunGame_dangomusi/Assets/Title/SceneController.cs
@@ -13,16 +13,78 @@ namespace RunGame.Title
     /// </summary>
     public class SceneController : MonoBehaviour
     {
+        /// <summary>
+        /// 「StageButton」の親オブジェクトを指定します。
+        /// </summary>
+        [SerializeField]
+        private Transform buttons = null;
+
+        /// <summary>
+        /// ボタン選択時の表示スケールを指定します。
+        /// </summary>
+        [SerializeField]
+        private Vector3 selectedScale = new Vector3(1.1f, 1.1f, 1);
+
+        // 現在選択されているボタンを示すインデックス
+        int selectedIndex = 0;
+
         // Start is called before the first frame update
         void Start()
         {
-
+            // GameControllerからステージ名一覧を取得
+            var stageNames = GameController.Instance.StageNames;
+            // buttons配列から各ボタンをループ処理
+            for (int index = 0; index < buttons.childCount; index++)
+            {
+                var button = buttons.GetChild(index);
+            }
         }
 
         // Update is called once per frame
         void Update()
         {
+            // 「Enter」キーが押された場合
+            if (Input.GetKeyUp(KeyCode.Return))
+            {
+                // 『ステージ画面』へシーン遷移
+                Stage.SceneController.StageNo = selectedIndex;
+                SceneManager.LoadScene("Stage 0");
+                return;
+            }
+            // 上カーソルキーが押された場合
+            else if (Input.GetKeyUp(KeyCode.UpArrow))
+            {
+                selectedIndex--;
+                if (selectedIndex < 0)
+                {
+                    selectedIndex = 0;
+                }
+            }
+            // 下カーソルキーが押された場合
+            else if (Input.GetKeyUp(KeyCode.DownArrow))
+            {
+                selectedIndex++;
+                if (selectedIndex > buttons.childCount - 1)
+                {
+                    selectedIndex = buttons.childCount - 1;
+                }
+            }
 
+            // buttons配列から各ボタンをループ処理
+            for (int index = 0; index < buttons.childCount; index++)
+            {
+                var button = buttons.GetChild(index);
+                // 選択中のボタンは拡大表示
+                if (index == selectedIndex)
+                {
+                    button.localScale = selectedScale;
+                }
+                // 非選択中のボタンは通常表示
+                else
+                {
+                    button.localScale = Vector3.one;
+                }
+            }
         }
 
         /// <summary>
