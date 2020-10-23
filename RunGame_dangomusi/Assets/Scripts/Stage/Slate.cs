168523c20a622e030a81be7486c10ee218370bd6
Miyoshi_Takuto <miyoshi.taku10@gmail.com>
2020-10-23 13:23:07 +0900

    看板のポップアップが７秒経過でも消えるように変更

--- RunGame_dangomusi/Assets/Scripts/Stage/Slate.cs
+++ RunGame_dangomusi/Assets/Scripts/Stage/Slate.cs
@@ -32,10 +32,19 @@ public class Slate : MonoBehaviour
     private void OnTriggerEnter2D(Collider2D collision) {
         if ( timer >= 1.0f ) {
             if ( collision.tag == "Player" ) {
+                StartCoroutine( "DeleteText" );
                 slatetext.text = text;
                 popupWindow.color = new Color( 0.0f, 0.0f, 0.0f, 0.5f );
-                Time.timeScale = 0.0f;
+                Time.timeScale = 0.01f;
             }
         }
+    }
+
+    IEnumerator DeleteText() {
+        yield return new WaitForSeconds( 0.07f );
+        slatetext.text = " ";
+        popupWindow.color = new Color( 0.0f, 0.0f, 0.0f, 0.0f );
+        Time.timeScale = 1.0f;
+        timer = 0;
     }
 }
